ee384495efbd2f78acd3130b3eadabf54c38c8b4
borovik51 <32337115+borovik51@users.noreply.github.com>
2023-05-20 12:20:04 +0300

    Update test_register.vhd

--- test_register.vhd
+++ test_register.vhd
@@ -1,62 +1,38 @@
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
 entity register_test is
 end register_test;
 
 architecture test_register_arch of register_test is 
-
-   signal d: std_logic_vector(1 to 24);
-   signal q: std_logic_vector(1 to 24);
-   signal clk, reset : std_logic;
-
-   begin
-      dut: entity work.reg(behavioral)
-      port map(d => d, q => q, clk => clk, reset => reset);
-      
-      stimulus: process is
-      begin
-	reset <= '1';
-	d <= "000000000000000000000000";
-	clk <= '1';
-	wait for 25 ns;
-	clk <= '0';
-	wait for 25 ns;
-        d <= "001000100010001000100010";
-	clk <= '1';
-	wait for 25 ns;
-	clk <= '0';
-	wait for 25 ns;
-        d <= "010001000100010001000100";
-	clk <= '1';
-	wait for 25 ns;
-	clk <= '0';
-	wait for 25 ns;
-        d <= "011001100110011001100110";
-	clk <= '1';
-	wait for 25 ns;
-	clk <= '0';
-	wait for 25 ns;
-        d <= "100010001000100010001000";
-	clk <= '1';
-	wait for 25 ns;
-	clk <= '0';
-	wait for 25 ns;
-        d <= "101010101010101010101010";
-	clk <= '1';
-	wait for 25 ns;
-	clk <= '0';
-	wait for 25 ns;
-        d <= "110011001100110011001100"; 
-	clk <= '1';
-	wait for 25 ns;
-	clk <= '0';
-	wait for 25 ns;
-        d <= "101110111011101110111011";
-	clk <= '1';
-	wait for 25 ns;
-	clk <= '0';
-	wait;
-   end process stimulus;
+    signal d: std_logic_vector(1 to 24);
+    signal q: std_logic_vector(1 to 24);
+    signal clk, reset : std_logic;
+    -- Clock period definitions
+    constant Tpc : time := 50ns;
+begin
+    dut: entity work.register_24_bit(behavioral)
+        port map(d => d, q => q, clk => clk, reset => reset);
+    
+    clock : process is
+    begin
+        clk <= '0';
+        wait for Tpc/2;
+        clk <= '1';
+        wait for Tpc/2;
+    end process clock;
+    
+    stimulus : process is
+    begin
+        reset <= '1'; wait for Tpc/2;
+        reset <= '0'; wait for Tpc/2;
+        d <= x"000000";
+        wait for Tpc/2;
+        --Start testing
+        for i in 0 to 100 loop
+            d <= std_logic_vector(unsigned(d) + 1);
+            wait for Tpc; 
+        end loop;
+    end process stimulus;
 end test_register_arch;
-

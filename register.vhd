4f1b7c4dfb08a3f06068f705a4b06da80b4eb20d
borovik51 <32337115+borovik51@users.noreply.github.com>
2023-05-20 19:13:23 +0300

    Update register.vhd
    updeted generic parametr
    now makes after reset 
    0 & def_value & 0...00

--- register.vhd
+++ register.vhd
@@ -2,7 +2,7 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity register_24_bit is
-    generic( defaulte_value : std_logic );
+    generic( defaulte_value : std_logic := '1' );
 	port ( d : in std_logic_vector(0 to 23);
 	       q : out std_logic_vector(0 to 23);
 	       clk, reset : in std_logic );
@@ -11,10 +11,10 @@ end entity register_24_bit;
 architecture behavioral of register_24_bit is
 begin
 	behavior : process (d, clk, reset) is
-		constant def_value : std_logic_vector(0 to 23) := (others => defaulte_value);
 	begin
 		if reset = '1' then
-			q <= def_value;
+			q(0 to 1) <= '0' & defaulte_value;
+			q(2 to 23) <= (others => '0');
 		elsif rising_edge(clk) then
 			q <= d;
 		end if;
